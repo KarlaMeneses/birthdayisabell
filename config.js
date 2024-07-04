2ba769b1a7531564376659b1f3393aa0be815fca
Karla Meneses <Lmkj3pa7@hotmail.com>
2024-07-04 10:53:19 -0400

    Happy birthday

---
+++ config.js
@@ -0,0 +1,88 @@
+// // 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// var config = {
+//     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+//     // 每句话尽量不要超过15个字,不然展示效果可能不太好
+//     texts: [
+//         "aaaaaaaa",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+//         "bbbbbbbbbb",  // 同上...
+//         "cccccccccc",
+//         "dddddddddddd",
+//         "eeeeeeeeee",
+//         "ffffffffff",
+//         "gggggggggg",
+//         "hhhhhhhhhhhh",
+//         "iiiiiiiii",
+//         "jjjjjjjjjjjj",
+//         "YAMI~~",
+//     ],
+//     /**
+//      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+//      * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+//      * 例如
+//      * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+//      *
+//      * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+//      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+//      */
+//     imgs: {
+//         "心爱的小可爱": "./imgs/xiaokeai.png",
+//         // "今天是你的生日": "./imgs/birthday.jpg",
+//     },
+//     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+//     desc: {
+//         turn_on: "turn_on",
+//         play: "play",
+//         bannar_coming: "bannar_coming",
+//         balloons_flying: "balloons_flying",
+//         cake_fadein: "cake_fadein?",
+//         light_candle: "light_candle?",
+//         wish_message: "wish_message",
+//         story: "A MESSAGE FOR YOU",
+//     }
+// };
+
+
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // Puedes escribir tantas frases como quieras, pero intenta que cada una no supere los 15 caracteres
+    // Esto es para asegurar que se vean bien en pantalla
+    texts: [
+        "Hoy es un día especial,", 
+        "¡Feliz cumpleaños!", 
+        "Te deseo mucho amor,", 
+        "y alegría en tu vida.", 
+        "Espero que todos tus", 
+        "sueños se hagan realidad.", 
+        "Disfruta de tu día,", 
+        "rodeado de tus seres", 
+        "queridos y amigos.", 
+        "¡Felicidades!", 
+        "Con cariño, TEAM DEV - SOFTCAR"
+    ],
+    /**
+     * Las imágenes son opcionales, pero si las incluyes, deben seguir el formato:
+     * "Texto exacto de arriba": "Ruta de la imagen"
+     * Por ejemplo
+     * "¡Feliz cumpleaños!": "./imgs/felizcumple.png"
+     *
+     * Si no quieres una imagen, simplemente comenta la línea con dos barras // al inicio
+     * Por ejemplo, la imagen de "Hoy es un día especial" no se mostrará si está comentada
+     */
+    imgs: {
+        "¡Feliz cumpleaños!": "./imgs/isabel_lozano.jpg",
+        // "Hoy es un día especial,": "./imgs/diaespecial.jpg",
+    },
+    // Descripciones de los botones, en español
+    desc: {
+        turn_on: "Encender",
+        play: "Reproducir",
+        bannar_coming: "Mostrar banner",
+        balloons_flying: "Soltar globos",
+        cake_fadein: "Aparecer pastel",
+        light_candle: "Encender vela",
+        wish_message: "Mensaje de deseos",
+        story: "UN MENSAJE PARA TI"
+    }
+};
+
+
